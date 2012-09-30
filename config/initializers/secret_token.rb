761d2ce476acd89b29c9d38929771a029223e630
Ciaran Lee <ciaran.lee@gmail.com>
2012-09-30 19:04:43 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Irishproperty::Application.config.secret_token = '356fd8bd59a1e1a48fb1fdd7469f83129db71f427bc38f43848ead20c6e53683a067a0d388d9e7fcc4f19211ed75859593305be6d6a08209a2de8006b192ce1f'
