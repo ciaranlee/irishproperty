054af303159850ade546dcaee4f8e37c27049a00
Ciaran Lee <ciaran.lee@gmail.com>
2012-10-21 17:45:16 +0100

    cleaning up some controler code

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -27,19 +27,29 @@ class SalesController < ApplicationController
   def stats
     filter_sales
     set_title('statistics')
-    @start = Time.parse '2010/01'
-    @end = Time.now.at_beginning_of_month
-    @months = []
-    @sales_stats = []
-    t = @start
-    while t < @end
+    @months, @sales_stats = stats_for @sales
+  end
+
+  def show
+    @sale = Sale.find(params[:id])
+  end
+
+  private
+
+  def stats_for(sales)
+    start = Time.parse '2010/01'
+    finish = Time.now.at_beginning_of_month
+    months = []
+    sales_stats = []
+    t = start
+    while t < finish
       month = t..(t+1.month - 1)
-      @months << month
+      months << month
       t += 1.month
 
-      monthly_sales = @sales.where(:date => month)
+      monthly_sales = sales.where(:date => month)
       prices = monthly_sales.select(:price).all.collect(&:price)
-      @sales_stats << {
+      sales_stats << {
         :sales => monthly_sales,
         :transaction_count => monthly_sales.count,
         :average => monthly_sales.average(:price).to_i,
@@ -52,10 +62,7 @@ class SalesController < ApplicationController
         :variance => prices.variance.to_i
       }
     end
-  end
-
-  def show
-    @sale = Sale.find(params[:id])
+    [months, sales_stats]
   end
 
   def load_counties
