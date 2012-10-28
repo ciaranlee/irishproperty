b688610e64fa9cf53cbcd5d115a85d031f34b29d
Ciaran Lee <ciaran.lee@gmail.com>
2012-10-28 09:54:47 +0000

    no longer allow ordering by county

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -6,8 +6,6 @@ class SalesController < ApplicationController
     order_hash = {
       'price-asc' => 'price ASC',
       'price-desc' => 'price DESC',
-      'county-asc' => 'county ASC',
-      'county-desc' => 'county DESC',
       'full_market_price-asc' => 'full_market_price ASC',
       'full_market_price-desc' => 'full_market_price DESC',
       'date-asc' => 'date ASC',
